8a25dbbeb1c3b40ef212a7c2e6b7d18ec110db55
Rafael Queiroz Vilaça <112505616+rafaqueiroz10@users.noreply.github.com>
2025-09-13 09:20:10 -0300

    Alterações na enum NaipesCarta

--- Entidades/src/main/java/br/trucomineiro/NaipesCarta.java
+++ Entidades/src/main/java/br/trucomineiro/NaipesCarta.java
@@ -1,5 +1,18 @@
 package br.trucomineiro;
 
 public enum NaipesCarta {
-    COPAS, OUROS, PAUS, ESPADAS;
+    COPAS("Copas"),
+    OUROS("Ouros"),
+    PAUS("Paus"),
+    ESPADAS("Espadas");
+
+    private final String nome;
+
+    NaipesCarta(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
 }
